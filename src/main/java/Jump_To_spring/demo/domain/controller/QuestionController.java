22ab33e3f8a5d28c6d1bc18fbbab3240d4201ccc
yangwoohyeon <dngus0717@gmail.com>
2025-04-08 18:40:27 +0900

    Spring Validation

--- src/main/java/Jump_To_spring/demo/domain/controller/QuestionController.java
+++ src/main/java/Jump_To_spring/demo/domain/controller/QuestionController.java
@@ -3,12 +3,13 @@ package Jump_To_spring.demo.domain.controller;
 import Jump_To_spring.demo.domain.Entity.Question;
 import Jump_To_spring.demo.domain.Repository.QuestionRepository;
 import Jump_To_spring.demo.domain.service.QuestionService;
+import Jump_To_spring.demo.question.QuestionForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,4 +34,17 @@ public class QuestionController {
         return "question_detail";
     }
 
+    @GetMapping(value="/create")
+    public String questionCreate(){
+        return "question_form";
+    }
+
+    @PostMapping(value="/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(),questionForm.getContent());
+        return "redirect:/question/list";
+    }
 }
--- src/main/java/Jump_To_spring/demo/domain/service/QuestionService.java
+++ src/main/java/Jump_To_spring/demo/domain/service/QuestionService.java
@@ -6,6 +6,7 @@ import Jump_To_spring.demo.domain.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,13 @@ public class QuestionService {
         }else{
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content){
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question);
     }
 }
